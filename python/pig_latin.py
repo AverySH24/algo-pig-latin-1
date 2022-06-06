4260d88c798ee1339c147449a6a2524e580b9e29
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-07 00:01:44 +0200

    working on it

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,11 @@
-def translate(word_or_phrase):
-  pass
+import re
 
+def translate(word_or_phrase):
+  phrase = word_or_phrase.split(" ")
+  result = ""
+  # print(re.search('^[aeiou]',word_or_phrase))
+  for i in phrase:
+    if re.search('^[aeiou]',i):
+      result+= 
+
+print(translate('apple'))
